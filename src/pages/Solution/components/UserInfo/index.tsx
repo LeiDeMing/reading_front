1ce3c7d3587edf6d68970f968801847d65b45d80
LeiDeMing <136371773@qq.com>
2020-03-25 16:09:13 +0800

    add

---
+++ src/pages/Solution/components/UserInfo/index.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { Avatar, Card } from '@alifd/next';
+import { store as appStore } from 'ice';
+
+const UserInfo = () => {
+  const [userInfo, userActions] = appStore.useModel('user');
+  useEffect(() => {
+    userActions.fetchUserProfile();
+  }, []);
+  
+  return (
+    <Card free>
+      <Card.Header
+        title="状态管理 - 全局状态"
+      />
+      <Card.Divider />
+      <Card.Content>
+        <Avatar size="small" src={userInfo.avatar} />
+        <span style={{ marginLeft: 10 }}>{userInfo.name}</span>
+      </Card.Content>
+    </Card>
+  );
+};
+
+export default UserInfo;
